451623a4dc7ed2a4e3eedb3d87c30750c5c6b472
超盐酸钠 <hyperhcl@126.com>
2024-05-20 18:40:19 +0800

    Added jiaochabianyi (not passed compile)

--- src/FreeRTOS/build.sh
+++ src/FreeRTOS/build.sh
@@ -1,7 +1,10 @@
+rm build/*
+
 bindgen src/wrapper.c -o src/bindings.rs -- -I src/include -I src/portable/GCC/ARM_CA53_64_RaspberryPi3
 aarch64-none-elf-as -mcpu=cortex-a53 -c -o build/FreeRTOS_asm_vector.o src/FreeRTOS_asm_vector.S
 aarch64-none-elf-as -mcpu=cortex-a53 -c -o build/startup.o src/startup.S
 aarch64-none-elf-as -mcpu=cortex-a53 -c -o build/portASM.o src/portable/GCC/ARM_CA53_64_RaspberryPi3/portASM.S
-cargo rustc -- --emit=obj --target=aarch64-unknown-none -o build/FreeRTOS_asm_vector.o
+cargo +nightly rustc --target=aarch64-unknown-none -Zbuild-std -- --emit=obj -o build/FreeRTOS.o
+cp $(find build/FreeRTOS-*.o) build/FreeRTOS.o
 
-# aarch64-none-elf-gcc -Wl,--build-id=none -std=gnu11 -T raspberrypi3.ld -o $@ -ffreestanding -O2 -nostdlib build/FreeRTOS_asm_vector.o build/startup.o build/portASM.o
+aarch64-none-elf-gcc -Wl,--build-id=none -std=gnu11 -T raspberrypi3.ld -o build/FreeRTOS.elf -ffreestanding -O2 -nostdlib build/FreeRTOS_asm_vector.o build/startup.o build/portASM.o build/FreeRTOS.o
--- src/FreeRTOS/src/main.rs
+++ src/FreeRTOS/src/main.rs
@@ -1,14 +1,10 @@
-#![no_std]
+#![feature(restrict_std)]
 #![no_main]
 use core::panic::PanicInfo;
 // include!("uart.rs");
+include!("bindings.rs");
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
-    loop {}
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
