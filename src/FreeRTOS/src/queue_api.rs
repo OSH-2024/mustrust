4357f9c9bf221d6719f1644c791169d7539dac6e
Zhenyu-Bo <bzy1117@mail.ustc.edu.cn>
2024-06-29 16:38:27 +0800

    修改一些函数名以符合命名规范

--- src/FreeRTOS/src/queue_api.rs
+++ src/FreeRTOS/src/queue_api.rs
@@ -1,82 +1,95 @@
+// queue_api.rs, queue APIs
+// This file is created by Ning Yuting.
+// To solve the issue of mutability of queue.
+
 use crate::port::*;
 use crate::queue::*;
 use crate::queue_h::*;
 use core::cell::UnsafeCell;
 
-pub struct QueueHandle_t<T>(UnsafeCell<QueueDefinition<T>>)
-    where
-        T: Default + Clone;
-// send, sync is used for sharing queue among threads
-unsafe impl<T: Default + Clone> Send for QueueHandle_t<T> {}
-unsafe impl<T: Default + Clone> Sync for QueueHandle_t<T> {}
+pub struct Queue<T>(UnsafeCell<QueueDefinition<T>>)
+where
+    T: Default + Clone;
 
-impl<T> QueueHandle_t<T>
-    where
-        T: Default + Clone,
+// send, sync is used for sharing queue among threads
+unsafe impl<T: Default + Clone> Send for Queue<T> {}
+unsafe impl<T: Default + Clone> Sync for Queue<T> {}
+
+impl<T> Queue<T>
+where
+    T: Default + Clone,
 {
-    pub fn xQueueCreate(length: UBaseType_t) -> Self {
-        QueueHandle_t(UnsafeCell::new(QueueDefinition::new(
+    pub fn new(length: UBaseType) -> Self {
+        Queue(UnsafeCell::new(QueueDefinition::new(
             length,
             QueueType::Base,
         )))
     }
-    pub fn xQueueSend(&self, pvItemToQueue: T, xTicksToWait: TickType) -> Result<(), QueueError> {
+
+    pub fn send(&self, pvItemToQueue: T, xTicksToWait: TickType) -> Result<(), QueueError> {
         unsafe {
             let inner = self.0.get();
-            (*inner).xQueueGenericSend(pvItemToQueue, xTicksToWait, queueSEND_TO_BACK)
+            (*inner).queue_generic_send(pvItemToQueue, xTicksToWait, queueSEND_TO_BACK)
         }
     }
 
-    pub fn xQueueSendToFront(
+    pub fn send_to_front(
         &self,
         pvItemToQueue: T,
         xTicksToWait: TickType,
     ) -> Result<(), QueueError> {
         unsafe {
             let inner = self.0.get();
-            (*inner).xQueueGenericSend(pvItemToQueue, xTicksToWait, queueSEND_TO_FRONT)
+            (*inner).queue_generic_send(pvItemToQueue, xTicksToWait, queueSEND_TO_FRONT)
         }
     }
-    pub fn xQueueSendToBack(&self, pvItemToQueue: T, xTicksToWait: TickType) -> Result<(), QueueError> {
+
+    pub fn send_to_back(&self, pvItemToQueue: T, xTicksToWait: TickType) -> Result<(), QueueError> {
         unsafe {
             let inner = self.0.get();
-            (*inner).xQueueGenericSend(pvItemToQueue, xTicksToWait, queueSEND_TO_BACK)
+            (*inner).queue_generic_send(pvItemToQueue, xTicksToWait, queueSEND_TO_BACK)
         }
     }
-    pub fn xQueueOverwrite(&self, pvItemToQueue: T) -> Result<(), QueueError> {
+
+    pub fn overwrite(&self, pvItemToQueue: T) -> Result<(), QueueError> {
         unsafe {
             let inner = self.0.get();
-            (*inner).xQueueGenericSend(pvItemToQueue, 0, queueOVERWRITE)
+            (*inner).queue_generic_send(pvItemToQueue, 0, queueOVERWRITE)
         }
     }
-    pub fn xQueueSendToFrontFromISR(&self, pvItemToQueue: T) -> (Result<(), QueueError>, bool) {
+
+    pub fn send_to_front_from_isr(&self, pvItemToQueue: T) -> (Result<(), QueueError>, bool) {
         unsafe {
             let inner = self.0.get();
-            (*inner).xQueueGenericSendFromISR(pvItemToQueue, queueSEND_TO_FRONT)
+            (*inner).queue_generic_send_from_isr(pvItemToQueue, queueSEND_TO_FRONT)
         }
     }
-    pub fn xQueueSendToBackFromISR(&self, pvItemToQueue: T) -> (Result<(), QueueError>, bool) {
+
+    pub fn send_to_back_from_isr(&self, pvItemToQueue: T) -> (Result<(), QueueError>, bool) {
         unsafe {
             let inner = self.0.get();
-            (*inner).xQueueGenericSendFromISR(pvItemToQueue, queueSEND_TO_BACK)
+            (*inner).queue_generic_send_from_isr(pvItemToQueue, queueSEND_TO_BACK)
         }
     }
-    pub fn xQueueOverwriteFromISR(&self, pvItemToQueue: T) -> (Result<(), QueueError>, bool) {
+
+    pub fn overwrite_from_isr(&self, pvItemToQueue: T) -> (Result<(), QueueError>, bool) {
         unsafe {
             let inner = self.0.get();
-            (*inner).xQueueGenericSendFromISR(pvItemToQueue, queueOVERWRITE)
+            (*inner).queue_generic_send_from_isr(pvItemToQueue, queueOVERWRITE)
         }
     }
-    pub fn xQueueReceive(&self, xTicksToWait: TickType) -> Result<T, QueueError> {
+
+    pub fn receive(&self, xTicksToWait: TickType) -> Result<T, QueueError> {
         unsafe {
             let inner = self.0.get();
-            (*inner).xQueueGenericReceive(xTicksToWait, false)
+            (*inner).queue_generic_receive(xTicksToWait, false)
         }
     }
-    pub fn xQueuePeek(&self, xTicksToWait: TickType) -> Result<T, QueueError> {
+
+    pub fn peek(&self, xTicksToWait: TickType) -> Result<T, QueueError> {
         unsafe {
             let inner = self.0.get();
-            (*inner).xQueueGenericReceive(xTicksToWait, true)
+            (*inner).queue_generic_receive(xTicksToWait, true)
         }
     }
 }
